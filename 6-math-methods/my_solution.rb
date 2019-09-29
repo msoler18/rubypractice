742b2e20d3747f5467a37d64cb3dc4ec350d283c
msoler18 <msoler735@gmail.com>
2019-09-29 16:45:11 -0500

    Solve aritchmetic operations inside methods challenge

--- 6-math-methods/my_solution.rb
+++ 6-math-methods/my_solution.rb
@@ -1,1 +1,24 @@
 # Math Methods
+
+#add
+def add(num1,num2)
+  return num1 + num2
+end
+
+#Substract
+def subtract(num1,num2)
+  return num1 - num2
+end
+
+#multiply
+def multiply(num1,num2)
+  return num1 * num2
+end
+
+#divide
+
+def divide(num1,num2)
+  return num1.to_f / num2.to_f
+end
+
+#puts subtract(10,7)
