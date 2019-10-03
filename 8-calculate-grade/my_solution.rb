af6120830fe7ffffab9997d19160de56886410ec
msoler18 <msoler735@gmail.com>
2019-10-02 19:54:46 -0500

    Solve get grade challenge

--- 8-calculate-grade/my_solution.rb
+++ 8-calculate-grade/my_solution.rb
@@ -1,3 +1,16 @@
 # Calculate a Grade
 
 # Tu solucion abajo:
+def get_grade(num)
+  if(num >= 90)
+    return 'A'
+  elsif( num >= 80 && num < 90)  
+    return 'B'
+  elsif(num >= 70 && num < 80) 
+    return 'C'
+  elsif(num >= 60 && num < 70)
+    return 'D'
+  else
+    return 'F' 
+  end      
+end
